9a20da76d4d4a21248d3b19dcb620a709c792de2
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2018-08-09 16:20:20 +0300

    Site update

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-function main(){
-    var dec = window.atob("bWFpbA==") + window.atob("dG86Y29udGFjdA==") + window.atob("QG5vdmFnZS5jb20udWE=");
-    document.getElementById("email").href = dec;
+function main() {
+    const email = window.atob("bWFpbA==") + window.atob("dG86Y29udGFjdA==") + window.atob("QG5vdmFnZS5jb20udWE=");
+    const emailElement = document.getElementById("email");
+    emailElement.href = email;
+    emailElement.innerHTML = email.replace("mailto:", "");
 }
