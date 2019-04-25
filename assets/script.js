6beacbef6c9dedd64fedaffcb74f540ff716d23c
Andriy Lysnevych <andriy.lysnevych@novage.com.ua>
2019-04-25 14:19:40 +0300

    Updated site

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,24 @@ function main() {
     const emailElement = document.getElementById("email");
     emailElement.href = email;
     emailElement.innerHTML = email.replace("mailto:", "");
+
+    document.querySelector('#navbar .navbar-toggle').addEventListener('click', menuToggle);
+
+    subMenuToggle();
+}
+
+function menuToggle() {
+    const menu = document.querySelector('#navbar .menu');
+    menu.classList.toggle('show');
+}
+
+function subMenuToggle() {
+    const menuItem = document.querySelectorAll('#navbar .menu-item');
+    menuItem.forEach(item => item.addEventListener('click', menuItemToggle));
+}
+
+function menuItemToggle () {
+    const subMenu = this.querySelector('.sub-menu');
+    subMenu.classList.toggle('show');
+    this.classList.toggle('switch');
 }
