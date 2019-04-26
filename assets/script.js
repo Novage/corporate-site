ab21359d4302f6d888b3f440d4553cd5f951dd00
Anton Shchemelev <anton.shchemelev@ukr.net>
2019-04-26 15:55:26 +0300

    Updated site

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,18 @@ function main() {
     const email = window.atob("bWFpbA==") + window.atob("dG86Y29udGFjdA==") + window.atob("QG5vdmFnZS5jb20udWE=");
     const emailElement = document.getElementById("email");
     emailElement.href = email;
-    emailElement.innerHTML = email.replace("mailto:", "");
+    emailElement.innerHTML = "<span>".concat(email.replace("mailto:", ""), "</span>");
 
-    document.querySelector('#navbar .navbar-toggle').addEventListener('click', menuToggle);
+    document.querySelector('#navbar .navbar-toggle').addEventListener('click', function () {
+        const menu = document.querySelector('#navbar .menu');
+        menu.classList.toggle('show');
+        const html=document.querySelector('html');
+        html.classList.toggle('show');
+    });
 
-    subMenuToggle();
-}
-
-function menuToggle() {
-    const menu = document.querySelector('#navbar .menu');
-    menu.classList.toggle('show');
-}
-
-function subMenuToggle() {
-    const menuItem = document.querySelectorAll('#navbar .menu-item');
-    menuItem.forEach(item => item.addEventListener('click', menuItemToggle));
-}
-
-function menuItemToggle () {
-    const subMenu = this.querySelector('.sub-menu');
-    subMenu.classList.toggle('show');
-    this.classList.toggle('switch');
+    document.querySelectorAll('#navbar .menu-item').forEach(item => item.addEventListener('click', function () {
+        const subMenu = this.querySelector('.sub-menu');
+        subMenu.classList.toggle('show');
+        this.classList.toggle('switch');
+    }));
 }
