d73f8f10d3c604ac62b276bae86ff994648ca0d0
DimaDemchenko <demchenkodmytro73@gmail.com>
2024-04-12 14:58:49 +0300

    nodePolyfills

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [nodePolyfills()],
+  },
   build: {
     format: "preserve",
   },
